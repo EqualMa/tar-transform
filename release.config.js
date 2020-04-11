8bb9de6420354fed422f4d24e11e0f7632b1b48f
EqualMa <equalma@outlook.com>
2020-04-12 00:09:34 +0800

    fix: release: updated version not synced to git

--- release.config.js
+++ release.config.js
@@ -7,8 +7,9 @@ module.exports = {
     [
       "@semantic-release/exec",
       {
-        prepareCmd: "node scripts/prepublish.js",
-        publishCmd: "node scripts/postpublish.js",
+        // https://semantic-release.gitbook.io/semantic-release/support/faq#how-can-i-use-a-npm-build-script-that-requires-the-package-jsons-version
+        prepareCmd:
+          "node scripts/prepublish.js && node scripts/postversion.js -v ${nextRelease.version}",
       },
     ],
     "@semantic-release/git",
--- scripts/postversion.js
+++ scripts/postversion.js
@@ -1,25 +1,29 @@
 /**
- * This script file will be executed AFTER the package is published
+ * This script file will be executed AFTER the new version is generated
  * (before the `git push`)
  *
  * The following procedures are included:
  *
- * 1. Before this process, `dist/package.json` is updated with the new version
- *    and the updates should be synced to `package.json` and git,
+ * 1. Before this process, the new version has been generated
+ *    and it should be synced to `package.json` and git,
  *    as described in [semantic-release/npm](https://github.com/semantic-release/npm#examples).
  *
  */
 
 const { readFile, writeFile } = require("fs").promises;
 
-const inputPkg = "dist/package.json";
 const outputPkg = "package.json";
 
 async function main() {
-  const distPkg = await readFile(inputPkg, "utf-8");
-  const { version } = JSON.parse(distPkg);
+  if (process.argv[2] !== "-v") {
+    throw new Error(
+      `postversion.js executed with wrong args: ${process.argv.join(" ")}`,
+    );
+  }
+
+  const version = process.argv[3];
   if (!version || typeof version !== "string") {
-    throw new Error(`version in ${inputPkg} is invalid: ${version}`);
+    throw new Error(`new version is invalid: ${version}`);
   }
 
   const pkg = await readFile(outputPkg, "utf8");
